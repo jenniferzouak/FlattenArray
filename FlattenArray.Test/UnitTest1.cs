89bde1a81a01a7187e272ce2309e0f00cedfe871
jenniferzouak <53r3n17yNOW@h4cksf0rc00k13s.com>
2019-01-24 13:10:34 -0700

    Initial version - this works

---
+++ FlattenArray.Test/UnitTest1.cs
@@ -0,0 +1,52 @@
+using System;
+using Xunit;
+
+namespace FlattenArray.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Flattener f = new Flattener();
+            object[] testArray = new object[]
+            {};
+            var rc = 
+            f.Flatten(testArray);
+            Assert.True(rc.Length == 0, "Length is incorrect, should be 0");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Flattener f = new Flattener();
+            object[] testArray = new object[]
+            { 1, 2, 3, 4 };
+            var rc =
+            f.Flatten(testArray);
+            Assert.True(rc.Length == 4, "Length is incorrect, should be 4");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Flattener f = new Flattener();
+            object[] testArray = new object[]
+            { 1, new object[] { } , 3, 4 };
+            var rc =
+            f.Flatten(testArray);
+            Assert.True(rc.Length == 3, "Length is incorrect, should be 3");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Flattener f = new Flattener();
+            object[] testArray = new object[]
+            { 1, new object[] { 2,3, new object[] { 6,7,8} } , 4, 5 };
+            var rc =
+            f.Flatten(testArray);
+            Assert.True(rc.Length == 8, "Length is incorrect, should be 8");
+        }
+    }
+}
---
+++ FlattenArray/Flattener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlattenArray
+{
+    public class Flattener
+    {
+        public object[] Flatten(object[] input) 
+        {
+            if (input == null) return null;
+           
+            List<object> outputList = new List<object>();
+            foreach(object item in input)
+            {
+                if (item == null) continue;
+                if (item.GetType().IsArray == true)
+                {
+                    object[] tmpArray = Flatten(item as object[]);
+                    outputList.AddRange(tmpArray);
+                }
+                else
+                {
+                    outputList.Add(item);
+                }
+
+            }
+            return outputList.ToArray();
+
+        }
+    }
+}
